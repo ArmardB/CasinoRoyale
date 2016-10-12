948889aa57bbdb75445e8757eec3a8f050ecdd38
Roberto DeDeus <rpdfil@gmail.com>
2016-10-12 16:49:09 -0400

    Stubbed out Storage interface and FileStorage class.

---
+++ src/main/java/io/royale7/casino/FileStorage.java
@@ -0,0 +1,12 @@
+package io.royale7.casino;
+
+public class FileStorage implements Storage {
+
+    public void loadPlayersInformation() {
+        return;
+    }
+
+    public void savePlayersInformation() {
+        return;
+    }
+}
