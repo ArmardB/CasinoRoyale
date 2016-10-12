57a6a2c63e5bd337a882b7d2471b13a0bd6f8841
Jonathan Leczner <jleczner@udel.edu>
2016-10-12 17:41:50 -0400

    added stubs and test class

---
+++ src/test/java/io/royale7/casino/PlayerManagerTest.java
@@ -0,0 +1,30 @@
+package io.royale7.casino;
+
+import org.junit.Test;
+
+/**
+ * Created by jonathanleczner on 10/12/16.
+ */
+public class PlayerManagerTest {
+    PlayerManager pm = new PlayerManager();
+
+    @Test
+    public void loginTest() {
+
+    }
+
+    @Test
+    public void loginFailTest() {
+
+    }
+
+    @Test
+    public void getDefaultPlayerTest() {
+
+    }
+
+    @Test
+    public void createNewPlayerTest() {
+
+    }
+}
