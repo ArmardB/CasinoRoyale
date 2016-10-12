9881c22a108885b6d518d1f1fd7aaf7c7e8dca63
Jonathan Leczner <jleczner@udel.edu>
2016-10-12 19:52:16 -0400

    added tests

--- src/test/java/io/royale7/casino/PlayerManagerTest.java
+++ src/test/java/io/royale7/casino/PlayerManagerTest.java
@@ -2,6 +2,8 @@ package io.royale7.casino;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by jonathanleczner on 10/12/16.
  */
@@ -10,12 +12,16 @@ public class PlayerManagerTest {
 
     @Test
     public void loginTest() {
-
+        Player expected = new Player("Naz", "higgdiggly");
+        Player actual = pm.login(0, "higgdiggly");
+        assertEquals(expected, actual);
     }
 
     @Test
     public void loginFailTest() {
-
+        Player expected = new Player("Naz", "higgdiggly");
+        Player actual = pm.login(0, "billybob");
+        assertEquals(expected, actual);
     }
 
     @Test
