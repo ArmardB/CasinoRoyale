3df3b1d63c2fa4fedcc5bea1c6aa48937ede8370
Sujatha Ravikumar <sujatharavikumar@Sujatha-ZC-25.local>
2016-10-11 20:06:05 -0400

    Initial Commit

---
+++ src/main/java/io/royale7/casino/App.java
@@ -0,0 +1,12 @@
+package io.royale7.casino;
+
+/**
+ * Created by sujatharavikumar on 10/11/16.
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/io/royale7/casino/AppTest.java
@@ -0,0 +1,7 @@
+package io.royale7.casino;
+
+/**
+ * Created by sujatharavikumar on 10/11/16.
+ */
+public class AppTest {
+}
