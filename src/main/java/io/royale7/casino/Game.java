81acc29a8c8d54e91b5858d2fefe40440d77dbc1
Carina Blair <carinablair@Carina-ZCW-5.fios-router.home>
2016-10-12 23:42:15 -0400

    Finished Game and Card Game

--- src/main/java/io/royale7/casino/Game.java
+++ src/main/java/io/royale7/casino/Game.java
@@ -1,9 +1,6 @@
 package io.royale7.casino;
 
-<<<<<<< HEAD
-public abstract class Game {
-    public int gameID;
-=======
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,16 +8,11 @@ import java.util.List;
  * Created by robertodedeus on 10/12/16.
  */
 public abstract class Game {
-    private int id;
-    private List<Player> playerCollection;
->>>>>>> 957bdde6f3a454b4d63d946656fc34d9c6f92a1b
+    private int gameID;
+
 
     public void init(){
 
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 957bdde6f3a454b4d63d946656fc34d9c6f92a1b
 }
