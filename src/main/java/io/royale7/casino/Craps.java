e29e1bcfcb39e02680ba89fa1c45fac190b901d6
nathanielholloway <n8talent@gmail.com>
2016-10-13 00:56:24 -0400

    GameManager started

--- src/main/java/io/royale7/casino/Craps.java
+++ src/main/java/io/royale7/casino/Craps.java
@@ -3,5 +3,6 @@ package io.royale7.casino;
 /**
  * Created by nathanielholloway on 10/12/16.
  */
-public class Craps {
+public class Craps extends Game {
+
 }
--- src/test/java/io/royale7/casino/AppTest.java
+++ src/test/java/io/royale7/casino/AppTest.java
@@ -1,7 +1,13 @@
 package io.royale7.casino;
 
+import org.junit.Test;
+
 /**
  * Created by sujatharavikumar on 10/11/16.
  */
 public class AppTest {
+    @Test
+    void mainTest(){
+        Casino casino = new Casino();
+    }
 }
