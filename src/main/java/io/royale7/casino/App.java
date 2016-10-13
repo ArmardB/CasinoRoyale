6c30f3ec5e1dac2053306b4bf55f24f0b96712a9
Carina Blair <carinablair@Carina-ZCW-5.fios-router.home>
2016-10-12 23:52:20 -0400

    Fixed formatting again

--- src/main/java/io/royale7/casino/App.java
+++ src/main/java/io/royale7/casino/App.java
@@ -6,13 +6,8 @@ package io.royale7.casino;
 public class App {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println();
-
-=======
         Casino casino = new Casino();
         casino.init();
->>>>>>> e3af49463974f823c588fc7d34a2742b906e4dd9
     }
 
 }
