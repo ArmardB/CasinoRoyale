b849fedd973ee12e50154b07a68a7e747c88dd3b
Jonathan Leczner <jleczner@udel.edu>
2016-10-13 19:27:00 -0400

    got slot test class back

--- src/test/java/io/royale7/casino/SlotsTest.java
+++ src/test/java/io/royale7/casino/SlotsTest.java
@@ -1,19 +1,49 @@
 package io.royale7.casino;
 
+import io.royale7.casino.Slots.Slot;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class SlotsTest {
     Slots s;
+    List<Player> playerList;
 
     @Before
     public void init() {
-        s = new Slots();
+        playerList = new ArrayList<>();
+        playerList.add(new Player("jon", "123"));
+        s = new Slots(playerList);
     }
 
     @Test
     public void setupTest() {
+//        s.init();
+        assertNotNull(s.getPlayer());
+    }
 
+    @Test
+    public void determineWinningsTest() {
+        Slot[][] slots = s.getSlots();
+        slots[1] = new Slot[]{Slot.SEVEN, Slot.SEVEN, Slot.SEVEN};
+        int expected = 500;
+        s.determineWinnings();
+        assertEquals(expected, s.getWinAmount());
+    }
+
+    @Test
+    public void slotsTest() {
+        double enterBalance = s.getPlayer().getAccountBalance();
+//        s.init();
+        double exitBalance = s.getPlayer().getAccountBalance();
+        assertNotEquals(enterBalance, exitBalance);
     }
 
 }
