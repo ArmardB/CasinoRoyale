4f36f33cd123c666f36a8967e8a7273c12cfb2b5
Armard Bellamy <armardbellamy@Armard-ZCW3.local>
2016-10-12 23:22:00 -0400

    Undo of erroneous changes

--- src/main/java/io/royale7/casino/Engine.java
+++ src/main/java/io/royale7/casino/Engine.java
@@ -8,7 +8,7 @@ public class Engine {
     UserManager userManager;
     GameManager gameManager;
 
-   
+
     Engine() {
         userManager = new UserManager();
         gameManager = new GameManager();
