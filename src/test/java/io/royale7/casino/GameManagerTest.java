289ee278fd11ec7faa2b6ca469653b6d055ce695
Sujatha Ravikumar <sujatharavikumar@Sujatha-ZC-25.local>
2016-10-13 11:00:19 -0400

    Modified GameManager class

---
+++ src/test/java/io/royale7/casino/GameManagerTest.java
@@ -0,0 +1,18 @@
+package io.royale7.casino;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by sujatharavikumar on 10/13/16.
+ */
+public class GameManagerTest {
+
+    @Test
+    public void getGameTest(){
+        GameManager gameManager = new GameManager();
+        Game game = gameManager.getGame(1);
+        Assert.assertTrue(game instanceof Craps);
+    }
+
+}
