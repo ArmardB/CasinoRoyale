93cef09d5f7c35a99f1e1494d4bf9eb71916ab55
Jonathan Leczner <jleczner@udel.edu>
2016-10-13 19:21:36 -0400

    re-added slots from fresh repo

--- src/main/java/io/royale7/casino/Slots.java
+++ src/main/java/io/royale7/casino/Slots.java
@@ -1,45 +1,170 @@
 package io.royale7.casino;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
-public class Slots extends Game {
+import static io.royale7.casino.Slots.Slot.getRandomSlotValue;
 
-    private SlotsDisplay display;
+public class Slots extends LuckGame {
+
+    protected enum Slot {
+        CHERRIES,   // x1
+        BAR,        // x2.5
+        SEVEN;      // x5
+
+        private static final List<Slot> VALUES = Collections.unmodifiableList(Arrays.asList(Slot.values()));
+        private static final int SIZE = VALUES.size();
+        public static final Random RANDOM = new Random();
+
+        public static Slot getRandomSlotValue() {
+            return VALUES.get(RANDOM.nextInt(SIZE));
+        }
+
+        // http://stackoverflow.com/questions/1972392/java-pick-a-random-value-from-an-enum
+    }
+
+    private SlotsDisplay slotsDisplay;
     private Player player;
 
     private boolean running;
-    private String inputString;
-    private String outputString;
-    private String[] slotsValues;
+    private boolean prompted;
+    private Slot[][] slotsValues;
+    private final int NUMREELS = 3;
     private int winAmount;
 
-    public Slots() {
+    public Slots(List<Player> playerList) {
+        super(playerList);
+        slotsDisplay = new SlotsDisplay();
+        slotsValues = new Slot[NUMREELS][NUMREELS];
+        player = playerList.get(0);
+    }
+
+    @Override
+    public void settle() {
 
     }
 
-    public boolean isRunning() { return running; }
+    @Override
+    public void bet() {
 
-    public void init(List<Player> playerList) { return; }
+    }
 
-    public void gameLoop() { return; }
+    protected boolean isRunning() { return running; }
+    public SlotsDisplay getDisplay() { return slotsDisplay; }
+    public Player getPlayer() { return player; }
+    protected Slot[][] getSlots() { return slotsValues; }
+    protected int getWinAmount() { return winAmount; }
 
-    public void playSlots() { return; }
+    public void init() { // entry and exit point
+        running = true;
+        prompted = false;
+        winAmount = 0;
+        for (Slot[] rows : slotsValues) {
+            for (Slot slot : rows) {
+                slot = getRandomSlotValue();
+            }
+        }
+        gameLoop();
+    }
 
-    private String getRandomSlotValue() { return null; }
+    public void gameLoop() {
+        while (running) {
+            if (prompted) {
+                playOrQuit(UserInput.promptString());
+                determineWinnings();
+            } else {
+                slotsDisplay.printUserInfo();
+                slotsDisplay.printWelcomeMenu();
+                prompted = true;
+            }
+        }
+    }
 
-    private String getSlotsValue(int index) { return null; }
+    public void playOrQuit(String input) {
+        switch (input) {
+            case "1":
+                playSlots();
+                break;
+            case "2":
+                quit();
+                break;
+            default:
+                Display.invalidSelection();
+                break;
+        }
+    }
 
-    public int determineWinnings() { return 0; }
+    public void playSlots() {
+        for (Slot[] rows : slotsValues) {
+            for (Slot slot : rows) {
+                slot = getRandomSlotValue();
+            }
+        }
+    }
 
-    public void quit() { return; }
+    public void determineWinnings() {
+        boolean won = true;
+        Slot[] payoutLine = slotsValues[1]; // just middle for now
+        Slot checkValue = payoutLine[0];
+
+        for (int i = 1; i < payoutLine.length; i++) {
+            if (payoutLine[i] != checkValue) {
+                won = false;
+            }
+        }
+        if (won) {
+            switch (checkValue) {
+                case SEVEN:
+                    winAmount = 500;
+                    break;
+                case CHERRIES:
+                    winAmount = 100;
+                    break;
+                case BAR:
+                    winAmount = 250;
+                    break;
+            }
+        }
+    }
+
+    public void quit() {
+        Display.logOutMenu();
+        running = false;
+    }
 
     private class SlotsDisplay extends Display {
-        private String welcomeMenu =
-                "Welcome my son to Slooooooooooooooooooooots. What do?\n" +
-                "[1] Play\n" +
-                "[2] Quit\n";
-        private String userInfo =
-                player.getName() + "\n" +
-                player.getAccountBalance();
+        private void printWelcomeMenu() {
+            outputLn("Welcome my son to Slooooooooooooooooooooots. What do?");
+            outputLn("[1] Play - $100");
+            outputLn("[2] Quit");
+        }
+
+        private void printUserInfo() {
+            outputLn(player.getName());
+            Display.printFormatBalance(player.getAccountBalance());
+        }
+
+        private void printSlots() {
+            for (Slot[] rows : slotsValues) {
+                for (Slot slot : rows) {
+                    System.out.printf("[%s]", slot.name());
+                }
+                System.out.println();
+            }
+        }
+
+        private void printWinnings() {
+            outputLn("I guess you won...");
+            System.out.print("+");
+            Display.printFormatBalance(winAmount);
+        }
+
+        private void printEndPrompt() {
+            outputLn("Now what?");
+            outputLn("[1] Play - $100");
+            outputLn("[2] Quit");
+        }
     }
 }
