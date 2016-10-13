85e7cd28d162047344b7015c4b8f5f6a362515d2
Carina Blair <carinablair@Carina-ZCW-5.fios-router.home>
2016-10-12 23:49:32 -0400

    Reformatted some classes

--- src/main/java/io/royale7/casino/Deck.java
+++ src/main/java/io/royale7/casino/Deck.java
@@ -3,20 +3,14 @@ package io.royale7.casino;
 import java.util.ArrayList;
 import java.util.List;
 
-<<<<<<< HEAD
-public class Deck {
-    public List<Card> cards = new ArrayList<Card>();
-=======
 /**
  * Created by sujatharavikumar on 10/12/16.
  */
 public class Deck {
 
-    private List<Card> cards = new ArrayList<Card>();
-    private Card singleCard;
+    public List<Card> cards = new ArrayList<Card>();
 
 
->>>>>>> 8266d196dc8bc60bafd62daeb8a1ec143233a4c5
     public Deck(){
         for(Rank rank: Rank.values()){
             for(Suit suit: Suit.values()){
@@ -24,9 +18,10 @@ public class Deck {
             }
         }
     }
-<<<<<<< HEAD
-}
-=======
+    
+
+
+
 
     public List<Card> getCards(){
         return this.cards;
@@ -36,6 +31,5 @@ public class Deck {
         return cards.get(index);
     }
 
-
 }
->>>>>>> 8266d196dc8bc60bafd62daeb8a1ec143233a4c5
+
---
+++ src/main/java/io/royale7/casino/Suit.java
@@ -0,0 +1,10 @@
+package io.royale7.casino;
+
+/**
+ * Created by sujatharavikumar on 10/12/16.
+ */
+public enum Suit {
+
+    Spades, Clubs, Hearts, Diamonds;
+
+}
