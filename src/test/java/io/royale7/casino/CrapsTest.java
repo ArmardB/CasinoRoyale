0dc1d23d04d7f3ecce2f5228221773d89660354c
nathanielholloway <n8talent@gmail.com>
2016-10-13 14:45:18 -0400

    updated Craps

---
+++ src/test/java/io/royale7/casino/CrapsTest.java
@@ -0,0 +1,16 @@
+package io.royale7.casino;
+
+import org.junit.Test;
+
+/**
+ * Created by nathanielholloway on 10/13/16.
+ */
+public class CrapsTest {
+
+    @Test
+    public void rollDiceTest(){
+       GameManager manager = new GameManager();
+        Craps craps = new Craps();
+        //int actual = craps.
+    }
+}
