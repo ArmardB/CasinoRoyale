c71874afc73883b7a57c84248a0ee31a54ed08d5
nathanielholloway <n8talent@gmail.com>
2016-10-14 10:31:02 -0400

    updated Craps

--- src/test/java/io/royale7/casino/HiLowTest.java
+++ src/test/java/io/royale7/casino/HiLowTest.java
@@ -4,37 +4,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-<<<<<<< HEAD
-///**
-// * Created by armardbellamy on 10/12/16.
-// */
-//public class HiLowTest {
-//
-//    HighLow highLow;
-//    Deck deck;
-//
-//    @Before
-//    public void init(){
-//       highLow = new HighLow();
-//        deck = new Deck();
-//    }
-//
-//    @Test
-//    public void dealACardTest(){
-//        Suit expected = Suit.Spades;
-//        Suit actual = deck.cards.get(0).getSuit();
-//        Assert.assertEquals("Should return card", expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void playAgainTest(){
-//
-//    }
-//
-//}
-=======
-/**
+
+/*
  * Created by armardbellamy on 10/12/16.
  */
 public class HiLowTest {
@@ -63,4 +34,3 @@ public class HiLowTest {
     }
 
 }
->>>>>>> 2be96e52ed3f623098acceabc9ba586df61807c6
